77cbfe1a678df25d6f76fe355a7f74e5909f6d64
Cyrill Krähenbühl <cyrill.k@hotmail.com>
2020-04-28 13:42:26 +0200

    Add xkeysnail keybindings

--- data/config/xkeysnail/config.py
+++ data/config/xkeysnail/config.py
@@ -37,85 +37,42 @@ define_keymap(re.compile("Firefox|Google-chrome"), {
     K("C-o"): [K("C-a"), K("C-c"), launch(["gedit"]), sleep(0.5), K("C-v")]
 }, "Firefox and Chrome")
 
-# Keybindings for Zeal https://github.com/zealdocs/zeal/
-define_keymap(re.compile("Zeal"), {
-    # Ctrl+s to focus search area
-    K("C-s"): K("C-k"),
-}, "Zeal")
+# # Keybindings for Zeal https://github.com/zealdocs/zeal/
+# define_keymap(re.compile("Zeal"), {
+#     # Ctrl+s to focus search area
+#     K("C-s"): K("C-k"),
+# }, "Zeal")
 
-# Emacs-like keybindings in non-Emacs applications
-define_keymap(re.compile("Evince"), {
-    # Cursor
-    # K("C-b"): with_mark(K("left")),
-    # K("C-f"): with_mark(K("right")),
-    # K("C-p"): with_mark(K("up")),
-    # K("C-n"): with_mark(K("down")),
-    K("C-h"): with_mark(K("backspace")),
-    # Forward/Backward word
-    K("M-b"): with_mark(K("C-left")),
-    K("M-f"): with_mark(K("C-right")),
-    # Beginning/End of line
-    K("C-a"): with_mark(K("home")),
-    K("C-e"): with_mark(K("end")),
-    # Page up/down
-    K("M-v"): with_mark(K("page_up")),
-    K("C-v"): with_mark(K("page_down")),
-    # Beginning/End of file
-    K("M-Shift-comma"): with_mark(K("C-home")),
-    K("M-Shift-dot"): with_mark(K("C-end")),
-    # Newline
-    K("C-m"): K("enter"),
-    K("C-j"): K("enter"),
-    K("C-o"): [K("enter"), K("left")],
-    # Copy
-    K("C-w"): [K("C-x"), set_mark(False)],
-    K("M-w"): [K("C-c"), set_mark(False)],
-    K("C-y"): [K("C-v"), set_mark(False)],
-    # Delete
-    K("C-d"): [K("delete"), set_mark(False)],
-    K("M-d"): [K("C-delete"), set_mark(False)],
-    # Kill line
-    K("C-k"): [K("Shift-end"), K("C-x"), set_mark(False)],
-    # Undo
-    K("C-slash"): [K("C-z"), set_mark(False)],
-    K("C-Shift-ro"): K("C-z"),
-    # Mark
-    K("C-space"): set_mark(True),
-    K("C-M-space"): with_or_set_mark(K("C-right")),
-    # Search
-    # K("C-s"): K("F3"),
-    # K("C-r"): K("Shift-F3"),
-    K("M-Shift-key_5"): K("C-h"),
-    # Cancel
-    K("C-g"): [K("esc"), set_mark(False)],
-    # Escape
-    K("C-q"): escape_next_key,
-    # C-x YYY
-    K("C-x"): {
-        # C-x h (select all)
-        K("h"): [K("C-home"), K("C-a"), set_mark(True)],
-        # C-x C-f (open)
-        K("C-f"): K("C-o"),
-        # C-x C-s (save)
-        K("C-s"): K("C-s"),
-        # C-x k (kill tab)
-        K("k"): K("C-f4"),
-        # C-x C-c (exit)
-        K("C-c"): K("C-q"),
-        # cancel
-        K("C-g"): pass_through_key,
-        # C-x u (undo)
-        K("u"): [K("C-z"), set_mark(False)],
+# Keybindings for okular
+define_keymap(re.compile("okular"), {
+    # Go to line
+    K("M-g"): {
+        K("M-g"): K("C-g"),
     }
-}, "Emacs-like keys without text move and search")
+}, "okular go-to page")
 
-# Emacs-like keybindings in non-Emacs applications
+# Emacs-like navigation
 define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt", "Gnome-terminal", "Evince"), {
     # Cursor
     K("C-b"): with_mark(K("left")),
     K("C-f"): with_mark(K("right")),
     K("C-p"): with_mark(K("up")),
     K("C-n"): with_mark(K("down")),
+}, "Emacs-like navigation")
+
+# Emacs-like search
+# Emacs-like keybindings in non-Emacs applications
+define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt", "Gnome-terminal", "Evince"), {
+    # Search
+    # K("C-s"): K("F3"),
+    # K("C-r"): K("Shift-F3"),
+    K("C-s"): K("C-f"),
+    K("C-r"): K("Shift-F3"),
+    K("M-Shift-key_5"): K("C-h"),
+}, "Emacs-like search")
+
+# Emacs-like others
+define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt", "Gnome-terminal"), {
     K("C-h"): with_mark(K("backspace")),
     # Forward/Backward word
     K("M-b"): with_mark(K("C-left")),
@@ -129,6 +86,8 @@ define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt", "Gnome-termina
     # Beginning/End of file
     K("M-Shift-comma"): with_mark(K("C-home")),
     K("M-Shift-dot"): with_mark(K("C-end")),
+    K("M-KEY_102ND"): with_mark(K("C-home")),
+    K("M-Shift-KEY_102ND"): with_mark(K("C-end")),
     # Newline
     K("C-m"): K("enter"),
     K("C-j"): K("enter"),
@@ -148,12 +107,6 @@ define_keymap(lambda wm_class: wm_class not in ("Emacs", "URxvt", "Gnome-termina
     # Mark
     K("C-space"): set_mark(True),
     K("C-M-space"): with_or_set_mark(K("C-right")),
-    # Search
-    # K("C-s"): K("F3"),
-    # K("C-r"): K("Shift-F3"),
-    K("C-s"): K("C-f"),
-    K("C-r"): K("Shift-F3"),
-    K("M-Shift-key_5"): K("C-h"),
     # Cancel
     K("C-g"): [K("esc"), set_mark(False)],
     # Escape
